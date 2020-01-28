83b1a8a75c611a9eb0cfd132cfa8ec5e7fc51ebe
GitHubShawnKim <shawnhckim@gmail.com>
2020-01-27 20:24:19 -0800

    fraction

---
+++ data-structure/01_numeric/02_float/02_02_divmod.py
@@ -0,0 +1,16 @@
+
+# (7, 3)
+res = divmod(45, 6)
+print(res)
+
+# 100.0
+res = round(100.96, -2)
+print(res)
+
+# 100.96
+res = round(100.96, 2)
+print(res)
+
+# (11, 4)
+res = 2.75.as_integer_ratio()
+print(res)
